6f3f9caa6d3a426acd3641aa5ded4640ce05989d
krishna <krishnakushwaha743@gmail.com>
2024-05-06 21:29:28 +0530

    added env

--- backend/models/user.js
+++ backend/models/user.js
@@ -32,16 +32,3 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
-// const userSchema = new mongoose.Schema({
-//     username: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     password: {
-//         type: String,
-//         required: true
-//     }
-// });
-
-// module.exports = mongoose.model('User', userSchema);
